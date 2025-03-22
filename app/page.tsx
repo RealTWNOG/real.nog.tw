bfd8d9f40dc44c7c1ac2a36faab3792362dda3a9
gnehs <gnehs@gnehs.net>
2025-03-22 15:36:50 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
